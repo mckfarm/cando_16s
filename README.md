5f83fc2239a8014e77352e5c2631ef7f6d1a73fc
unknown <mckennafarmer2023@u.northwestern.edu>
2022-03-25 09:28:37 -0500

    v1v3 archiving, work on v4
    moving forward with v4 analysis based on Harmon's qiime pipeline work

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # CANDO+P reactor - 16s rRNA analysis
 This repo summarizes the workflow and scripts for analyzing 16s rRNA sequence data from the lab-scale CANDO+P reactor. DNA was extracted from reactor biomass and sequenced at the Rush Genomics Core and Microbiome Core Facility.
 
-16s rRNA PCR was performed with two different primer sets to compare the taxonomic results. The V1/V3 workflow uses the primers 27F/534R primers as recommended by (MiDAS version 4)[https://www.biorxiv.org/content/10.1101/2021.07.06.451231v1].
+As of March 25 2022, GitHub is organized by sequencing date:
 
-V1-V3 27F	AGAGTTTGATCCTGGCTCAG
-V1-V3 534R	ATTACCGCGGCTGCTGG
-
-The V4/V5 workflow uses the primers 515F/926R as recommended in (Parada et al 2016)[https://sfamjournals.onlinelibrary.wiley.com/doi/full/10.1111/1462-2920.13023].
+## December 21 summary
+16s rRNA PCR was performed with two different primer sets to compare the taxonomic results. The V4/V5 workflow uses the primers 515F/926R as recommended in (Parada et al 2016)[https://sfamjournals.onlinelibrary.wiley.com/doi/full/10.1111/1462-2920.13023]. This is what I will use going forward for community analysis.
 
 V4-V5 515F	GTGYCAGCMGCCGCGGTAA
 V4-V5 926R	CCGYCAATTYMTTTRAGTTT
+
+The V1/V3 workflow uses the primers 27F/534R primers as recommended by (MiDAS version 4)[https://www.biorxiv.org/content/10.1101/2021.07.06.451231v1]. These results weren't great because the length of the final amplified product wasn't long enough to overlap the forward and reverse reads reliably.
+
+V1-V3 27F	AGAGTTTGATCCTGGCTCAG
+V1-V3 534R	ATTACCGCGGCTGCTGG
---
+++ dec21/v4/scripts/qiime_results.R
@@ -0,0 +1,138 @@
+### packages and working directory ---------
+setwd("~/Github/cando_16s/v4")
+
+library(qiime2R)
+library(phyloseq)
+library(ggplot2)
+library(tidyverse)
+library(cowplot)
+library(lubridate)
+library(MetBrewer)
+library(ggcorrplot)
+
+physeq <- qza_to_phyloseq(
+  features="./qiime/rarefied_table.qza",
+  tree="./qiime/rooted_tree.qza",
+  taxonomy="./qiime/taxonomy_midas.qza",
+  metadata = "./qiime/mar01_metadata.txt"
+)
+
+physeq <- subset_taxa(physeq, !Genus=="Mitochondria" & !Genus=="Chloroplast")
+
+# relative abundance
+rel <- transform_sample_counts(physeq, function(x) x*100/sum(x))
+write.csv(rel@tax_table,"./results/all_tax.csv")
+
+topN <- 10
+rel_10_names <- sort(taxa_sums(rel), decreasing=TRUE)[1:topN]
+rel_10 <- prune_taxa(names(rel_10_names), rel)
+
+# specific genus of interest
+## nitrifiers, PAOs/GAOs
+rel_nit <- subset_taxa(rel,Genus=="Nitrotoga" | 
+                         Genus=="Nitrospira" | 
+                         Genus=="Nitrobacter" | 
+                         Genus=="Nitrosomonas" | 
+                         Genus=="Nitrosospira")
+rel_phos <- subset_taxa(rel,Genus=="Ca_Accumulibacter" | 
+                          Genus=="Tetrasphaera" |
+                          Genus=="Ca_Obscuribacter" |
+                          Genus=="Dechloromonas" |
+                          Genus=="Halomonas" |
+                          Genus=="Microlunatus" |
+                          Genus=="Ca_Competibacter" |
+                          Genus=="Ca_Contendobacter" |
+                          Genus=="Defluviicoccus"|
+                          Genus=="Micropruina"|
+                          Genus=="Propionvibrio")
+
+rel_nit <- tax_glom(rel_nit,"Genus")
+rel_phos <- tax_glom(rel_phos,"Genus")
+
+# prep for plotting with ggplot - convert to dataframe format
+rel_nit_df <- psmelt(rel_nit) 
+rel_phos_df <- psmelt(rel_phos)
+rel_10_df <- psmelt(rel_10)
+
+# date formatting
+rel_nit_df$date <- mdy(rel_nit_df$date)
+rel_phos_df$date <- mdy(rel_phos_df$date)
+rel_10_df$date <- mdy(rel_10_df$date)
+
+### correlation matrix PAO/GAO ------
+
+# subset columns
+rel_phos_df <- rel_phos_df %>% select("Abundance","date","Genus")
+
+# reshape to wide format for correlation function
+rel_phos_df_subset <- spread(rel_phos_df, key = Genus, value = Abundance) 
+write.csv(rel_phos_df_subset,"phos_taxa.csv")
+
+# save correlation matrix
+cor_rel_phos <- cor(select(rel_phos_df_subset,-c(date)))
+
+# save significance matrix
+cor_rel_phos_pmat <- cor_pmat(select(rel_phos_df_subset,-c(date)))
+
+### plotting ---------------
+# nitrifiers
+ggplot(data=rel_nit_df,mapping=aes(x=date,y=Abundance,fill=Genus)) + 
+  geom_bar(stat="identity") +   
+  theme_bw() + 
+  scale_fill_manual(values=met.brewer("Hiroshige", 3)) + 
+  theme(axis.text.x = element_text(angle = 0),legend.text = element_text(face = "italic")) +
+  scale_x_date(date_labels="%m-%y") +
+  ylab("Relative Abundance (%)") + 
+  xlab("Date (Month-Year)") +
+  labs(fill="Genus")
+
+ggsave("./results/relative_ab_nit.tiff",width=2500,height=1500,unit="px")
+
+# PAOs/GAOs
+ggplot(data=rel_phos_df,mapping=aes(x=date,y=Abundance,
+                                    fill=factor(Genus,levels=c("Ca_Accumulibacter","Tetrasphaera","Ca_Competibacter")))) +
+  geom_bar(stat="identity") +
+  theme_bw() +
+  scale_fill_manual(values=met.brewer("Hiroshige", 3)) +
+  theme(axis.text.x = element_text(angle = 0),legend.text = element_text(face = "italic")) +
+  scale_x_date(date_labels="%m-%y") +
+  ylab("Relative Abundance (%)") +
+  xlab("Date (Month-Year)") +
+  labs(fill="Genus")
+
+ggsave("./results/relative_ab_phos.tiff",width=3000,height=1500,unit="px")
+
+ggplot(data=rel_phos_df,mapping=aes(x=date,y=Abundance,
+                                    fill=factor(Genus,levels=c("Ca_Accumulibacter","Tetrasphaera","Ca_Obscuribacter","Dechloromonas","Halomonas","Microlunatus",
+                                                               "Ca_Competibacter","Ca_Contendobacter","Defluviicoccus","Micropruina","Propionvibrio")))) + 
+  geom_bar(stat="identity") + 
+  theme_bw() + 
+  scale_fill_manual(values=met.brewer("Tiepolo", 11)) + 
+  theme(axis.text.x = element_text(angle = 0),legend.text = element_text(face = "italic")) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %y") +
+  ylab("Relative Abundance (%)") + 
+  xlab("Date") + 
+  labs(fill="Genus")
+
+ggsave("relative_ab_phos.tiff",
+       width=3000,height=2000,unit="px",dpi=400)
+
+# top 10 abundant genus
+ggplot(data=rel_10_df,mapping=aes(x=date,y=Abundance,fill=Genus)) +
+  geom_bar(stat="identity") + 
+  theme_bw() + 
+  scale_fill_manual(values=met.brewer("Redon", 13)) + 
+  theme(axis.text.x = element_text(angle = 0),legend.text = element_text(face = "italic")) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %y") +
+  ylab("Relative Abundance (%)") + 
+  xlab("") + 
+  labs(fill="Genus")
+
+ggsave("./results/top10.tiff",width=3500,height=1500,unit="px",dpi=400)
+
+# correlation plots
+
+ggcorrplot(cor_rel_phos, p.mat=cor_rel_phos_pmat, 
+           type="lower", lab="true", insig="blank",colors=c("red","white","lightblue"))
+ggsave("./results/phos_corr.tiff",width=1200,height=1000,unit="px",scale=1.5)
+
