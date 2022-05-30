77ab8a281060af9f2164a25a95d944dd33b8d63d
unknown <mckennafarmer2023@u.northwestern.edu>
2022-05-29 19:14:19 -0500

    prospectus

--- dec21/v4/scripts/qiime_results.R
+++ dec21/v4/scripts/qiime_results.R
@@ -1,4 +1,4 @@
-### packages and working directory ---------
+# packages and working directory ---------
 setwd("~/Github/cando_16s/dec21/v4")
 
 library(qiime2R)
@@ -10,6 +10,7 @@ library(lubridate)
 library(MetBrewer)
 library(ggcorrplot)
 
+# data import --------
 physeq <- qza_to_phyloseq(
   features="./qiime/rarefied_table.qza",
   tree="./qiime/rooted_tree.qza",
@@ -17,17 +18,23 @@ physeq <- qza_to_phyloseq(
   metadata = "./qiime/mar01_metadata.txt"
 )
 
+phases <- data.frame(x1=ymd("2021-05-26"),x2=ymd("2021-08-06"),
+                     x3=ymd("2021-12-3"),x4=ymd("2022-04-04"),
+                     x5=ymd("2022-05-24"))
+
+# cleaning -------
+# removing eukaryotes
 physeq <- subset_taxa(physeq, !Genus=="Mitochondria" & !Genus=="Chloroplast")
 
 # relative abundance
 rel <- transform_sample_counts(physeq, function(x) x*100/sum(x))
-write.csv(rel@tax_table,"./results/all_tax.csv")
+# write.csv(rel@tax_table,"./results/all_tax.csv")
 
 topN <- 20
 rel_10_names <- sort(taxa_sums(rel), decreasing=TRUE)[1:topN]
 rel_10 <- prune_taxa(names(rel_10_names), rel)
 
-# specific genus of interest
+# specific functional groups -------
 ## nitrifiers, PAOs/GAOs
 rel_nit <- subset_taxa(rel,Genus=="Nitrotoga" | 
                          Genus=="Nitrospira" | 
@@ -36,15 +43,9 @@ rel_nit <- subset_taxa(rel,Genus=="Nitrotoga" |
                          Genus=="Nitrosospira")
 rel_phos <- subset_taxa(rel,Genus=="Ca_Accumulibacter" | 
                           Genus=="Tetrasphaera" |
-                          Genus=="Ca_Obscuribacter" |
                           Genus=="Dechloromonas" |
-                          Genus=="Halomonas" |
-                          Genus=="Microlunatus" |
                           Genus=="Ca_Competibacter" |
-                          Genus=="Ca_Contendobacter" |
-                          Genus=="Defluviicoccus"|
-                          Genus=="Micropruina"|
-                          Genus=="Propionvibrio")
+                          Genus=="Micropruina")
 
 rel_nit <- tax_glom(rel_nit,"Genus")
 rel_phos <- tax_glom(rel_phos,"Genus")
@@ -52,6 +53,8 @@ rel_phos <- tax_glom(rel_phos,"Genus")
 # prep for plotting with ggplot - convert to dataframe format
 rel_nit_df <- psmelt(rel_nit) 
 rel_phos_df <- psmelt(rel_phos)
+rel_phos_df$Genus <- factor(rel_phos_df$Genus,levels=c("Ca_Accumulibacter","Tetrasphaera","Dechloromonas",
+                                           "Ca_Competibacter","Micropruina"))
 rel_10_df <- psmelt(rel_10)
 
 # date formatting
@@ -59,26 +62,13 @@ rel_nit_df$date <- mdy(rel_nit_df$date)
 rel_phos_df$date <- mdy(rel_phos_df$date)
 rel_10_df$date <- mdy(rel_10_df$date)
 
-
 # subset columns
 rel_phos_df <- rel_phos_df %>% select("date","Abundance","Genus")
-rel_10_df <- rel_10_df %>% select("Abundance","date","Genus")
-write.csv(rel_10_df,"./results/10_taxa.csv")
+rel_10_df <- rel_10_df %>% select("date","Abundance","Genus")
+# write.csv(rel_10_df,"./results/10_taxa.csv")
 
-# reshape to wide format for correlation function
-rel_phos_df_subset <- spread(rel_phos_df, key = Genus, value = Abundance) 
-write.csv(rel_phos_df_subset,"./results/phos_taxa.csv")
 
-### correlation matrix PAO/GAO ------
-
-# save correlation matrix
-cor_rel_phos <- cor(select(rel_phos_df_subset,-c(date)))
-
-# save significance matrix
-cor_rel_phos_pmat <- cor_pmat(select(rel_phos_df_subset,-c(date)))
-
-### plotting ---------------
-# nitrifiers
+# plotting nitrifiers -----------
 ggplot(data=rel_nit_df,mapping=aes(x=date,y=Abundance,fill=Genus)) + 
   geom_bar(stat="identity") +   
   theme_bw() + 
@@ -88,24 +78,20 @@ ggplot(data=rel_nit_df,mapping=aes(x=date,y=Abundance,fill=Genus)) +
   ylab("Relative Abundance (%)") + 
   xlab("Date (Month-Year)") +
   labs(fill="Genus")
-
 ggsave("./results/relative_ab_nit.tiff",width=2500,height=1500,unit="px")
 
-# PAOs/GAOs
-ggplot(data=rel_phos_df,mapping=aes(x=date,y=Abundance,
-                                    fill=factor(Genus,levels=c("Ca_Accumulibacter","Tetrasphaera","Ca_Obscuribacter","Dechloromonas","Halomonas","Microlunatus",
-                                                               "Ca_Competibacter","Ca_Contendobacter","Defluviicoccus","Micropruina","Propionvibrio")))) + 
+# plotting PAOs/GAOs
+ggplot(data=rel_phos_df,mapping=aes(x=date,y=Abundance,fill=Genus)) + 
   geom_bar(stat="identity") + 
-  theme_bw() + 
-  scale_fill_manual(values=met.brewer("Tiepolo", 11)) + 
+  theme_classic() + 
+  scale_fill_manual(values=met.brewer("Tiepolo", 6)) + 
   theme(axis.text.x = element_text(angle = 0),legend.text = element_text(face = "italic")) +
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %y") +
-  ylab("Relative Abundance (%)") + 
-  xlab("Date") + 
-  labs(fill="Genus")
-
-ggsave("./results/relative_ab_phos.tiff",
-       width=3000,height=2000,unit="px",dpi=400)
+  labs(y="Relative Abundance [%]",x="Date") + 
+  geom_vline(xintercept=phases$x2,color="chocolate4") +
+  ylim(0,40) +
+  theme(legend.position="none")
+ggsave("./results/relative_ab_phos.tiff",width=7,height=4,units="in")
 
 # top 10 abundant genus
 ggplot(data=rel_10_df,mapping=aes(x=date,y=Abundance,fill=Genus)) +
@@ -120,9 +106,26 @@ ggplot(data=rel_10_df,mapping=aes(x=date,y=Abundance,fill=Genus)) +
 
 ggsave("./results/top10.tiff",width=3500,height=1500,unit="px",dpi=400)
 
-# correlation plots
 
-ggcorrplot(cor_rel_phos, p.mat=cor_rel_phos_pmat, 
-           type="lower", lab="true", insig="blank",colors=c("red","white","lightblue"))
-ggsave("./results/phos_corr.tiff",width=1200,height=1000,unit="px",scale=1.5)
+# correlation matrix PAO/GAO ------
+# reshape to wide format for correlation function
+rel_phos_df_subset <- spread(rel_phos_df, key = Genus, value = Abundance) 
+write.csv(rel_phos_df_subset,"./results/phos_taxa.csv")
+
+# save correlation matrix
+spear_rel_phos <- cor(select(rel_phos_df_subset,-c(date)),method="spearman")
+pear_rel_phos <- cor(select(rel_phos_df_subset,-c(date)),method="pearson")
+
+# save significance matrix
+spear_rel_phos_pmat <- cor_pmat(select(rel_phos_df_subset,-c(date)),method="spearman",exact=F)
+pear_rel_phos_pmat <- cor_pmat(select(rel_phos_df_subset,-c(date)),method="pearson")
+
+# correlation plots
+ggcorrplot(spear_rel_phos, p.mat=spear_rel_phos_pmat, 
+           type="lower", lab="true", sig.level=0.05,
+           insig="pch", colors=c("orangered3","white","lightblue"))
+ggsave("./results/phos_corr.tiff",width=4,height=3,unit="in",scale=1.5)
+
+
+
 
